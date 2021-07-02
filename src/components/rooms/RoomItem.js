696803c3ca3fcc705b4b2d6faf039eda73931ff6
vkishor18 <vanshikakishor00@gmail.com>
2021-07-03 03:46:52 +0530

    created room context

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -4,12 +4,12 @@ import TimeAgo from 'timeago-react';
 const RoomItem = () => {
   return (
     <div>
-      <div className="d-flex justify-content-between align-items-center">
+      <div className="d-flex justify-content-between w-100 align-items-center">
         <h3 className="text-disappear">Room name </h3>
         <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
       </div>
       <div className="d-flex align-items-center text-black-70">
-        <span>No messages yet.....</span>
+        <span>No messages yet..</span>
       </div>
     </div>
   );
