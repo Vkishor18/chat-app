d2d1caafb40c5978123691ecab95941fbe139b86
vkishor18 <vanshikakishor00@gmail.com>
2021-07-06 00:36:31 +0530

    showed the status of user: online or offline

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,12 +1,13 @@
-import { useCallback, useState , useEffect } from "react";
+import { useCallback, useState, useEffect } from 'react';
+import { database } from './firebase';
 
-export function useModalState(defaultValue = false){
-  const [ isOpen , setIsOpen ] = useState(defaultValue);
+export function useModalState(defaultValue = false) {
+  const [isOpen, setIsOpen] = useState(defaultValue);
 
-  const open = useCallback(()=> setIsOpen(true) ,[]);
-  const close = useCallback(()=> setIsOpen(false) ,[]);
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
 
-  return {isOpen , open , close};
+  return { isOpen, open, close };
 }
 
 export const useMediaQuery = query => {
@@ -26,3 +27,25 @@ export const useMediaQuery = query => {
 
   return matches;
 };
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+        setPresence(data);
+      }
+    });
+
+    return () => {
+      userStatusRef.off()
+    }
+  }, [uid]);
+
+  return presence;
+
+}
