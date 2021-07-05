8da4bf1c547838c65a4e1b9d7a57aea3a80b8ed4
vkishor18 <vanshikakishor00@gmail.com>
2021-07-05 16:10:38 +0530

    added real time presence

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,16 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const ProfileContext = createContext();
 
@@ -8,9 +19,14 @@ export const ProfileProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     let userRef;
+    let userStatusRef;
+
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
+
         userRef = database.ref(`/profile/${authObj.uid}`);
+
         userRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
           const data = {
@@ -23,10 +39,27 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setIsLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef) {
           userRef.off();
         }
+
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+
         setProfile(null);
         setIsLoading(false);
       }
@@ -35,6 +68,9 @@ export const ProfileProvider = ({ children }) => {
       authUnsub();
       if (userRef) {
         userRef.off();
+      }
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
