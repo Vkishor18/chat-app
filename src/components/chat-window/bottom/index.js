ed51d1df08eee34dc491296c6311493587c4f3d8
vkishor18 <vanshikakishor00@gmail.com>
2021-07-07 12:09:24 +0530

    added an option to record and upload audio files

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -5,6 +5,7 @@ import firebase from 'firebase/app';
 import { database } from '../../../misc/firebase';
 import { useProfile } from '../../../context/profile.context';
 import AttachmentBtnModal from './AttachmentBtnModal';
+import AudioMsgBtn from './AudioMsgBtn';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -103,6 +104,7 @@ const Bottom = () => {
     <div>
       <InputGroup>
         <AttachmentBtnModal afterUpload={afterUpload} />
+        <AudioMsgBtn afterUpload={afterUpload} />
         <Input
           placeholder="Write a new message here..."
           value={input}
